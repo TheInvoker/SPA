518a1ec05cf89275c6163ff6e6ee05cccb79ade4
TheInvoker <ryans.dsouza@mail.utoronto.ca>
2017-12-28 11:32:12 -0500

    update

---
+++ public/spa.js
@@ -0,0 +1,67 @@
+var SPA = new function() {
+	
+	var opened = {},
+		pages = [],
+		active = null;
+	
+	this.timestamp = () => new Date().valueOf();
+
+	this.addPages = function(user_pages) {
+		user_pages.forEach(p => pages.push(p));
+	};
+	
+	this.start = function() {
+		loadPageFromHash();
+	};
+	
+	this.openPage = function(stateObj, url) {
+		history.pushState(stateObj, "", url);
+		loadPageFromHash();
+	};
+
+	function loadPageFromHash() {
+		var path = window.location.pathname;
+		var url = window.location.pathname + window.location.hash + window.location.search;
+		var page = getPage(path);
+		var p = opened[url];
+		if (p) {
+			enablePage(page, p);
+		} else {
+			openPage(url, page);
+		}
+	}
+	
+	function getPage(path) {
+		var page = pages.find(p => p.path.toLowerCase() == path.toLowerCase());
+		return page || pages.find(p => p.default);
+	}
+	
+	function enablePage(page, p) {
+		pages.forEach(p => {
+			p.layout.style.display = "none";
+		});
+		page.layout.style.display = "block";
+		
+		page.context.childNodes.forEach(c => {
+			c.style.display = "none";
+		});
+		p.style.display = "block";	
+
+		if (active) active.close();
+		active = page;
+		page.open();		
+	}
+	
+	function openPage(id, page) {
+		page.content(p => {
+			opened[id] = p;
+			page.context.appendChild(p);
+			enablePage(page, p);
+		});
+	}
+	
+	window.addEventListener('popstate', event => {
+		//var data = event.state;
+		loadPageFromHash();
+	}, false);
+};
---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+
+// start express module
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+ *  Home page http request handler.
+ */
+
+app.get('/foo', function(req, res) {
+	//res.redirect('/osler/index.html');
+	res.header('Content-Type', "text/html; charset=utf-8");
+	res.sendFile(path.join(__dirname, 'public/foo.html'));
+});
+
+// start the server
+var server = app.listen(process.env.PORT || 3000, function () {
+	console.log("Server started...");
+});
